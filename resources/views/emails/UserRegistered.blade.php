1618266d3ff29b8f2bd9da6270abb717b02e7734
kushal-chavan <kushal.chavan120@gmail.com>
2019-10-04 22:16:53 +0530

    Mailing system

---
+++ resources/views/emails/UserRegistered.blade.php
@@ -0,0 +1,6 @@
+<h3>Hi, Admin</h3>
+<div><h3>New User Details</h3></div>
+<div><h3>Name: {{$name}} </h3></div>
+<div><h3>Email: {{$email}}</h3></div>
+<div><h3>Phone Number: {{$phonenumber}}</h3></div>
+<h3>Check More Details on gshatech dashboard</h3>
